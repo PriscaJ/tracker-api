61146cc760b5e9799e9a2cbc8800fe6cc48e543f
Prisca Joseph <priscajoseph@priscas-mbp.myfiosgateway.com>
2020-07-22 01:12:51 -0400

    First Commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const auth = require('./auth.js');
+const { connectToDb } = require('./db.js');
+const { installHandler } = require('./api_handler.js');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cookieParser());
+app.use('/auth', auth.routes);
+
+installHandler(app);
+
+const port = process.env.API_SERVER_PORT || 3000;
+
+(async function start() {
+  try {
+    await connectToDb();
+    app.listen(port, () => {
+      console.log(`API server started on port ${port}`);
+    });
+  } catch (err) {
+    console.log('ERROR:', err);
+  }
+}());
