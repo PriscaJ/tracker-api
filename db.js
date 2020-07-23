5567583b48898c08743169cd511bd68d9924bb6f
Prisca Joseph <priscajoseph@priscas-mbp.myfiosgateway.com>
2020-07-23 15:25:48 -0400

    mongo db uri

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const { MongoClient } = require('mongodb');
 let db;
 
 async function connectToDb() {
-  const url = process.env.MONGO_URI || 'mongodb://localhost/issuetracker';
+  const url = process.env.MONGODB_URI || 'mongodb://localhost/issuetracker';
   const client = new MongoClient(url, { useNewUrlParser: true });
   await client.connect();
   console.log('Connected to MongoDB at', url);
