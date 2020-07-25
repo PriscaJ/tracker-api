696e51e335674a18dd484463c5f32134437ad466
Prisca Joseph <priscajoseph@priscas-mbp.myfiosgateway.com>
2020-07-25 02:12:57 -0400

    lint

--- graphql_date.js
+++ graphql_date.js
@@ -1,7 +1,7 @@
 const { GraphQLScalarType } = require('graphql');
 const { Kind } = require('graphql/language');
 
-const GraphQLDate = new GraphQLScalarType({ 
+const GraphQLDate = new GraphQLScalarType({
   name: 'GraphQLDate',
   description: 'A Date() type in GraphQL as a scalar',
   serialize(value) {
@@ -12,7 +12,7 @@ const GraphQLDate = new GraphQLScalarType({
     return Number.isNaN(dateValue.getTime()) ? undefined : dateValue;
   },
   parseLiteral(ast) {
-    if (ast.kind == Kind.STRING) {
+    if (ast.kind === Kind.STRING) {
       const value = new Date(ast.value);
       return Number.isNaN(value.getTime()) ? undefined : value;
     }
